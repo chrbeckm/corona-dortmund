f144a6d4bf3854b1e8c51c5a829ad89b9a693de3
Christian Beckmann <christian.beckmann@tu-dortmund.de>
2021-08-13 19:10:04 +0200

    add new cases for is

--- python/insta_story.py
+++ python/insta_story.py
@@ -22,7 +22,13 @@ factor = 2
 fig, ax = plt.subplots(constrained_layout=True, figsize=(9 / factor, 16 / factor))
 
 ln1 = ax.bar(x, data["aktuell_erkrankte_Personen"][forerun:], label="Aktive Fälle")
-ax.set_ylabel("Aktive Fälle")
+ln2 = ax.bar(
+    x,
+    data["Zuwachs_positiver_Testergebnisse_zum_Vortag"][forerun:],
+    color="red",
+    label="pos. Fälle",
+)
+ax.set_ylabel("Aktive Fälle / pos. Fälle")
 ax.set_ylim(0, np.max(data["aktuell_erkrankte_Personen"][forerun:]) * 1.01)
 
 ax2 = ax.twinx()
@@ -31,11 +37,11 @@ ax.set_xlim(-0.7, -forerun - 0.3)
 ax.set_xticks(x[::7])
 ax.set_xticklabels(data["Datum"][forerun::7], rotation=45, ha="right")
 
-ln2 = ax2.plot(x, inzidenz[forerun:], ".", color="orange", label="7-Tage Inzidenz")
+ln3 = ax2.plot(x, inzidenz[forerun:], ".", color="orange", label="7-Tage Inzidenz")
 ax2.set_ylabel("7-Tage Inzidenz")
 ax2.set_ylim(0, np.max(inzidenz[forerun:]) * 1.01)
 
-fig.legend(loc=(0.135, 0.9), framealpha=1)
+fig.legend(loc=(0.135, 0.875), framealpha=1)
 ax.set_title("Covid-19 Zahlen aus Dortmund")
 ax2.grid()
 
